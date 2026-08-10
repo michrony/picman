26abeec9711dfb576dbdf6cf8af928b7f496d37c
Michael Maron <michrony@gmail.com>
2026-08-09 22:35:08 -0400

    Create picman.install.bat

---
+++ picman.install.bat
@@ -0,0 +1,15 @@
+rem version 2026-07-16
+rem run run after installing pytnon
+
+pip install iptcinfo3 
+pip install validators
+pip install pillow
+pip install tzdata
+pip install tzwhere
+pip install ExifRead
+pip install timezonefinder
+pip install arrow
+pip install mysql-connector-python 
+pip install httplib2
+
+pause
